7dca484b3c645fb8d8c701ebf1c247b76aede06b
yong il Kim <yongbamceman@gmail.com>
2015-01-12 15:08:38 +0900

    next step is check why other angular js not activate

---
+++ public_html/app.js
@@ -0,0 +1,51 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+'use strict';
+
+// declare modules
+angular.module('Authentication', []);
+angular.module('Home', []);
+
+angular.module('httpAuth', [
+    'Authentication',
+    'Home',
+    'ngRoute',
+    'ngCookies'
+])
+ 
+.config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider
+        .when('/login', {
+            controller: 'LoginController',
+            templateUrl: 'login.html',
+            hideMenus: true
+        })
+ 
+        .when('/', {
+            controller: 'HomeController',
+            templateUrl: 'home.html'
+        })
+ 
+        .otherwise({ redirectTo: '/login' });
+}])
+ 
+.run(['$rootScope', '$location', '$cookieStore', '$http',
+    function ($rootScope, $location, $cookieStore, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+        }
+ 
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in
+            if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
+                $location.path('/login');
+            }
+        });
+    }]);
+
---
+++ public_html/loginController.js
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+'use strict';
+ 
+angular.module('Authentication')
+ 
+.controller('LoginController',
+    ['$scope', '$rootScope', '$location', 'AuthenticationService',
+    function ($scope, $rootScope, $location, AuthenticationService) {
+        // reset login status
+        AuthenticationService.ClearCredentials();
+ 
+        $scope.login = function () {
+            $scope.dataLoading = true;
+            AuthenticationService.Login($scope.username, $scope.password, function(response) {
+                if(response.success) {
+                    AuthenticationService.SetCredentials($scope.username, $scope.password);
+                    $location.path('/');
+                } else {
+                    $scope.error = response.message;
+                    $scope.dataLoading = false;
+                }
+            });
+        };
+    }]);
